1964fa6003c958d964820ebfd60172cf5be86870
saba jokhadze <saba.joxadze.04@gmail.com>
2023-12-26 01:07:44 +0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FooterComponent,
+    HomeComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
