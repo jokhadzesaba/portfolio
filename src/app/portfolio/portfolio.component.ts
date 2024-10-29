160dd88b474a502b7c2a999ee347a7e53f37c3fb
jokhadzesaba <saba.joxadze.04@gmail.com>
2024-10-29 17:53:05 +0400

    depl

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -4,12 +4,15 @@ import { SwitchService } from '../switch.service';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
-  public imgUrls = ['assets/project3.jpg',
-  'assets/project1.jpg',
-  'assets/project2.jpg',
-  'assets/tic.png']
-  constructor(public mode:SwitchService){}
+  public imgUrls = [
+    'assets/image.png',
+    'assets/project3.jpg',
+    'assets/project1.jpg',
+    'assets/project2.jpg',
+    'assets/tic.png',
+  ];
+  constructor(public mode: SwitchService) {}
 }
